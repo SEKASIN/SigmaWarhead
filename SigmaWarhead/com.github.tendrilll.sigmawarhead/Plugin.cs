f5dab6d2f2da227ea0e6e944b1a4690f212a1fd6
Ten <32621403+TenDRILLL@users.noreply.github.com>
2023-07-20 19:13:44 +0300

    1.0.2

---
+++ SigmaWarhead/com.github.tendrilll.sigmawarhead/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Exiled.API.Features;
+using Log = Exiled.API.Features.Log;
+
+namespace SigmaWarhead.com.github.tendrilll.sigmawarhead {
+    public class SigmaWarhead : Plugin<Config> {
+        public override string Name => "SigmaWarhead";
+        public override string Author => "TenDRILLL";
+        public override Version Version => new Version(1, 0, 2);
+        public EventHandler EventHandler;
+
+        public override void OnEnabled() {
+            Log.Info("SigmaWarhead loading...");
+            if (!Config.IsEnabled) {
+                Log.Warn("SigmaWarhead disabled from config, unloading...");
+                OnDisabled();
+                return;                
+            }
+            EventHandler = new EventHandler(this);
+            Log.Info("SigmaWarhead loaded.");
+        }
+        
+        public override void OnDisabled() {
+            EventHandler.UnregisterEvents();
+            Log.Info("SigmaWarhead unloaded.");
+        }
+    }
+}
